2813b43d569abc8e47e757d990f390c6914da125
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-09-22 23:10:56 -0300

    Update function get locais

--- main/main.py
+++ main/main.py
@@ -1,73 +1,79 @@
 
 class Coisa:
-  def __init__(self, estado = None):
-    self.estado = estado
-  
-  #Isso aqui é para imprimir sem usar print
-  def __repr__(self):
-    #Objeto na forma de strinf
-    return '{}'.format(getattr(self, '__name__', self.__class__.__name__))
+    def __init__(self, estado=None):
+        self.estado = estado
 
-  def mostrar_estado(self):
-    ## Mostra o estado do agente
-    return str(self.estado)
+    def __repr__(self):
+        # Objeto na forma de strinf
+        return '{}'.format(getattr(self, '__name__', self.__class__.__name__))
 
-  def vivo(self):
-    return hasattr(self, 'vivo') and self.vivo
+    def mostrar_estado(self):
+        # Mostra o estado do agente
+        return str(self.estado)
+
+    def vivo(self):
+        return hasattr(self, 'vivo') and self.vivo
+
 
 class Agente(Coisa):
-  def __init__(self, estado = None, funcao_agente = None):
-    super().__init__(estado)
+    def __init__(self, nome, estado=None):
+        self.nome = nome
+        super().__init__(estado)
+        self.funcao_agente = ''
+        self.historico_percepcoes = []
 
-    if funcao_agente == None:
-      def acao(*entradas): #aqui é para indicar que podem ter várias entradas
-        return 'Ação padrão'
-      self.funcao_agente = funcao_agente
-      self.historico_percepcoes = []
-  
-  #Recebe do meio exterior. Pode ser de um sensor, mas nesse caso é do teclado
-  def percepcao(self):
-    entrada = input("Entre com as percepções")
-    self.historico_percepcoes.append(eval(entrada))
+# Esquerda, direita, aspirar, noOp
+    # def aspirar(self, local):
 
-  #mostra o resultado da função agente
-  def saida(self):
-    return self.funcao_agente(self.historico_percepcoes)
+        
 
+    # Recebe do meio exterior. Pode ser de um sensor, mas nesse caso é do teclado
+    def percepcao(self):
+        entrada = input("Entre com as percepções")
+        self.historico_percepcoes.append(eval(entrada))
+
+    # mostra o resultado da função agente
+    def saida(self):
+        return self.funcao_agente(self.historico_percepcoes)
 
 
-class Ambiente():
+class Ambiente(Coisa):
+    def __init__(self, estado_inicial=None):
+        self.estado_inicial = estado_inicial
+        # São literalmente objetos ao redor que não sejam agentes
+        self.objetos_no_ambiente = []
+        self.agentes = []
+        self.locais = {}
 
-  def __init__(self, estado_inicial = None):
-    self.estado_inicial = estado_inicial
-    #São literalmente objetos ao redor que não sejam agentes
-    self.objetos_no_ambiente = []
-    self.agentes = []
-    self.locais = {}
+    def percepcao(self, agente):
+        # Percepção do agente
+        return agente
 
-  def percepcao(self, agente):
-    #Percepção do agente
-    return agente
-  
-  def adicionar_agente(self, agente):
-    self.agentes.append(agente)
+    def adicionar_agente(self, agente):
+        self.agentes.append(agente)
 
-  def listar_agentes(self):
-    return self.agentes
-  
-  def adicionar_objeto(self, objeto):
-    self.objetos_no_ambiente.append(objeto)
+    def getAgentes(self):
+        print('Lista de agentes \n')
+        for i in self.agentes:
+            print(i.nome)
 
-  def adicionar_local(self, local):
-    local = local.upper()
-    if local in self.locais.keys():
-      print('O local já existe!')
-    
-    else:
-      self.locais[local] = self.estado_inicial
+    def adicionar_objeto(self, objeto):
+        self.objetos_no_ambiente.append(objeto())
 
-  def show_locais(self):
-    print(self.locais.keys())
+    def adicionar_local(self, local, conteudo):
+        local = local.upper()
+        if local in self.locais.keys() :
+            print('O local já existe!') 
+
+        if conteudo != 'sujo' and conteudo != 'limpo':
+            print('Local só poder ser sujo ou limpo')
+
+        else:
+            self.locais[local] = conteudo
+
+    def getLocais(self):
+        for local in self.locais.keys():
+            print(f'O local {local} está {self.locais[local]}')
 
 
 # Esquerda, direita, aspirar, noOp
@@ -75,13 +81,19 @@ class Ambiente():
 # adicionar chave: dicio[chave] = valor
 
 
-ambiente = Ambiente("Sujo")
-ambiente.adicionar_local('A')
-ambiente.adicionar_local('B')
-ambiente.adicionar_local('b')
-ambiente.adicionar_local('a')
+ambiente = Ambiente(True)
+print('Locais \n')
 
-ambiente.show_locais()
+ambiente.adicionar_local('A', 'sujo')
+ambiente.adicionar_local('B', 'sujo')
 
-agente = Agente(True, 'E')
+ambiente.getLocais()
+
+agente = Agente("agente1", True)
+agente2 = Agente("agente2", True)
+ambiente.adicionar_agente(agente)
+ambiente.adicionar_agente(agente2)
+
+
+
 
