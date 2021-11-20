125f4377ea1b601545287b38999f766c7ff13d58
Jayneanderson Santos <aprenderprogramacao001@gmail.com>
2021-11-20 01:24:49 -0300

    fix: update function to add indice

--- main/first_duplicate.py
+++ main/first_duplicate.py
@@ -1,9 +1,14 @@
+#Esse algoritmo é para encontrar o número que se repete primeiro. No caso, se houver mais de uma repetição o retornado será o que aparecer primeiro
+#Solução: ler o array e colocar todos os valores em um novo vetor até que um repetido apareça. O primeiro repetido que aparecer será nosso valor procurado
 def firstDuplicate(vetor):
     elements = []
-
+    indice = 0
     for i in vetor:
-        if(i in elements):
-            return vetor[i]
-        elements.append(vetor[i])
+        if(i in elements): 
+            return vetor[indice]
+        elements.append(vetor[indice])
+        indice = indice + 1
 
     return -1
+
+print(firstDuplicate([2,1,3,5,2,3]))
